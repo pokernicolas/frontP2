d49c057861025a99e76cf79cc4becc09e46479f5
pokernicolas <nicolasa7x@hotmail.com>
2021-06-16 22:32:52 -0300

    Produtos

---
+++ src/pages/produto/detalhes/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import './index.css';
+ 
+export default class produto extends Component {
+    state = {
+        produto: {},
+    };
+ 
+    componentDidMount() {
+        const { id } = this.props.match.params;
+ 
+        fetch(`${process.env.REACT_APP_API_URL}/${id}`)
+            .then(produto =>
+                produto.json().then(produto => this.setState({ produto }))
+            )
+            .catch(erro => this.setState({ erro }));
+    }
+ 
+    render() {
+        const { produto, index } = this.state;
+ 
+        return (
+            <div className="produto-info">
+                <h1> {produto.nome} </h1>
+                <h1> {produto.descricao} </h1>
+                <h1> {produto.preco} </h1>
+                <h1> {produto.qtdEstoque} </h1>
+                <br />
+                <Link to={`/produtos`}> Voltar </Link> <br />
+                <Link to={`/editarProduto/${produto.id}`}> Editar </Link> <br />
+                <Link to={`/deletarProduto/${produto.id}`}> Deletar </Link> <br />
+            </div >
+        );
+    }
+}
