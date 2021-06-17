113415d02906de00f12c93168d41049487847303
pokernicolas <nicolasa7x@hotmail.com>
2021-06-16 23:06:17 -0300

    produtop2

--- src/pages/produto/main/index.js
+++ src/pages/produto/main/index.js
@@ -14,7 +14,7 @@ export default class Main extends Component {
     }
  
     componentDidMount() {
-        fetch("${process.env.REACT_APP_API_URL}")
+        fetch(`${process.env.REACT_APP_API_URL}`)
             .then(produto =>
                 produto.json().then(produto => this.setState({ produto }))
             )
